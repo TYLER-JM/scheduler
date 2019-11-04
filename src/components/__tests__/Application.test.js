094187ee5810b95d64c081f0f6fda1e480e07d1e
tyler martin <t.mar92@gmail.com>
2019-11-04 15:28:06 -0500

    adds test for Application component and adds put key to mock axios object

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,22 +1,43 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getAllByTestId, getByPlaceholderText, getByAltText } from "@testing-library/react";
 
 import Application from "components/Application";
-import { exportAllDeclaration } from "@babel/types";
 
 afterEach(cleanup);
 
-xit("renders without crashing", () => {
-  render(<Application />);
+// xit("renders without crashing", () => {
+//   render(<Application />);
+// });
+
+describe("Application", () => {
+
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText("Monday"));  
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();   
+  });
+  
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+
+    // try {
+      await waitForElement(() => getByText(container, "Archie Cohen"));
+      const appointment = getAllByTestId(container, "appointment")[0]; 
+      // console.log(prettyDOM(appointment));
+
+      fireEvent.click(getByAltText(appointment, "Add"));
+      fireEvent.change(getByPlaceholderText(appointment, /enter your name here/i), {
+        target: {value: "Lydia Miller-Jones"}
+      });
+      fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+      fireEvent.click(getByText(appointment, "Save"));
+    // } 
+    // catch {
+    //   console.log("whhaaa");
+    // }
+  });
+
 });
-
-it("defaults to Monday and changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
-
-  await waitForElement(() => getByText("Monday"));  
-  fireEvent.click(getByText("Tuesday"));
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    
-});
-
