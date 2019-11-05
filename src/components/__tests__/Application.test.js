14da9c0b17ea26676f615a1dcd86acf7f07a6544
tyler martin <t.mar92@gmail.com>
2019-11-04 21:58:42 -0500

    adds tests for error handling

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getAllByTestId, getByPlaceholderText, getByAltText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getAllByTestId, getByPlaceholderText, getByAltText, queryByText, queryByAltText } from "@testing-library/react";
 
 import Application from "components/Application";
+
+import axios from "axios";
 
 afterEach(cleanup);
 
@@ -21,7 +23,7 @@ describe("Application", () => {
   });
   
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
     // try {
       await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -29,15 +31,129 @@ describe("Application", () => {
       // console.log(prettyDOM(appointment));
 
       fireEvent.click(getByAltText(appointment, "Add"));
+      // debug();
       fireEvent.change(getByPlaceholderText(appointment, /enter your name here/i), {
         target: {value: "Lydia Miller-Jones"}
       });
       fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
       fireEvent.click(getByText(appointment, "Save"));
+      expect(getByText(appointment, "Saving")).toBeInTheDocument();
+      await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+      ///////////
+      //TROUBLE//
+      // const day = getAllByTestId(container, "day").find(day => 
+      //     queryByText(day, "Monday")
+      //   );
+      //   console.log(prettyDOM(day))
+      // expect(day.lastChild.innerHTML).toBe("no spots remaining");
+
+      // console.log(prettyDOM(container));
+
     // } 
     // catch {
     //   console.log("whhaaa");
     // }
+  });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    const { container, debug } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    //1. Click the "delete" button on the selected appointment
+    const appointment = getAllByTestId(container, "appointment").find(appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+
+    expect(getByText(appointment, "Are you sure you want to delete")).toBeInTheDocument();
+
+    //2. click the "confirm button on the confirm mode"
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    //3. check that the element with the text "Deleting" is displayed
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
+    //4. wait until the empty mode appears
+    await waitForElement(() => getByAltText(appointment, "Add"));
+    //5. check that the DayListItem with the text "" also has the text 2 spots remaining
+
+    ///////////
+    //TROUBLE//
+    // const day = getAllByTestId(container, "day").find(day => 
+    //   queryByText(day, "Monday")
+    // );
+    // expect(day.lastChild.innerHTML).toBe("2 spots remaining");
+
+  });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    
+    const appointment = getAllByTestId(container, "appointment").find(appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+    
+    expect(getByText(appointment, "Save")).toBeInTheDocument();
+    
+    fireEvent.click(getByText(appointment, "Save"));
+    
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    
+    await waitForElement(() => getByText(appointment, "Archie Cohen"));
+
+    const day = getAllByTestId(container, "day").find(day => 
+      queryByText(day, "Monday")
+    );
+    expect(day.lastChild.innerHTML).toBe("1 spot remaining");
+
+  });
+
+  it("shows the save error when failing to save an appointment", async () => {
+    axios.put.mockRejectedValueOnce();
+    
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment")[0]; 
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter your name here/i), {
+      target: {value: "Lydia Miller-Jones"}
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Could not save"));
+    expect(getByText(appointment, "Could not save")).toBeInTheDocument();
+    
+  });
+  it("shows the delete error when failing to delete an existing appointment", async () => {
+    axios.delete.mockRejectedValueOnce();
+
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    //1. Click the "delete" button on the selected appointment
+    const appointment = getAllByTestId(container, "appointment").find(appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+
+    expect(getByText(appointment, "Are you sure you want to delete")).toBeInTheDocument();
+
+    //2. click the "confirm button on the confirm mode"
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    //3. check that the element with the text "Deleting" is displayed
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Could not delete"));
+    expect(getByText(appointment, "Could not delete")).toBeInTheDocument();
+
   });
 
 });
